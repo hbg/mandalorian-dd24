1e90184479ef38b49e37d44c483b5fd808060304
Harris Beg <harris@harrisbeg.com>
2024-05-11 01:47:31 -0700

    Cool pages

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return "<h1>Welcome to the home site.</h1>"
+    return render_template('index.html')
+
+@app.route("/code/<code_id>")
+def code_view(code_id):
+    return render_template('code.html', code_id=code_id)
